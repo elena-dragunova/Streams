2e1c2b64cee0969467302267f30807b823bdcc2e
elena-dragunova <elenak-8647@gmail.com>
2019-12-23 16:06:08 +0500

    Create main routes. Add header with auth button.

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+class GoogleAuth extends React.Component{
+  state = {
+    isSignedIn: null,
+  };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '151400219457-c6l20rjj6sgs26ndvh95lp09dl8tsise.apps.googleusercontent.com',
+        scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({
+          isSignedIn: this.auth.isSignedIn.get(),
+        });
+        this.auth.isSignedIn.listen(this.onAuthChange);
+      });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button className="ui red google button" onClick={this.onSignOutClick}>
+          <i className="icon google" />
+          Sign Out
+        </button>
+      );
+    }
+
+    return (
+      <button className="ui red google button" onClick={this.onSignInClick}>
+        <i className="icon google" />
+        Sign In with Google
+      </button>
+    );
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>
+  }
+}
+
+export default GoogleAuth;
